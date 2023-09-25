f08bc3bf0ce2c7f427151a179ba388c2e5d8529a
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-09-25 20:29:07 +0600

    commit --a Angular CLI

--- Angular CLI/FirstApp/src/app/app.component.ts
+++ Angular CLI/FirstApp/src/app/app.component.ts
@@ -12,9 +12,38 @@ export class AppComponent {
 currentColor: Color = Color.Red;
 Color = Color;
 products: Array<string> = ['banan', 'apple', 'orange'];
+size: number = 10;
+
+classes: any = {
+  "consolas": this.isAdmin,
+  "underline-text": this.isAdmin,
+  "red-text": !this.isAdmin
+};
+
+styles: any = {
+  "color": "green",
+  "background-color": "red"
+}
 
   onButtonClick(): void {
     this.title = 'Vladimir';
+    this.isAdmin = !this.isAdmin;
+    this.size += 5;
+
+    this.classes = {
+      "consolas": this.isAdmin,
+      "underline-text": this.isAdmin,
+      "red-text": !this.isAdmin
+    }
+
+      this.styles = {
+        "color": "red",
+        "background-color": "green"
+      }
+  }
+
+  onNumberClick(num: number): void {
+    this.title = this.products[num];
   }
 }
 
